aef16b8111245a788390679cc8423c292f8742c9
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-07-30 21:42:09 -0700

    Create reuseable dropdown toggle

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
-console.log('test');
+
+const dropdown = document.querySelectorAll('.dropdown');
+console.log(dropdown);
+dropdown.forEach(element => {
+  const dropdownToggle = element.querySelector('.dropdown-toggle');
+  const dropdownContent = element.querySelector('.dropdown-content');
+  dropdownToggle.addEventListener('click', function(){
+    if (dropdownContent.style.display === 'none'){
+      dropdownContent.style.display = 'block';
+    } else {
+      dropdownContent.style.display = 'none';
+    }
+  })
+});
