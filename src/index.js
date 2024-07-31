274b61c47ba2bab96ee345169ed144593807e4c4
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-07-31 16:39:23 -0700

    set dropdown position to absolute and close other dropdowns when new one is opened

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './style.css';
 
 const dropdown = document.querySelectorAll('.dropdown');
-console.log(dropdown);
 dropdown.forEach(element => {
   const dropdownToggle = element.querySelector('.dropdown-toggle');
   const dropdownContent = element.querySelector('.dropdown-content');
   dropdownToggle.addEventListener('click', function(){
+    dropdown.forEach(drop => {
+      const otherDropdownContent = drop.querySelector('.dropdown-content');
+      otherDropdownContent.style.display = 'none';
+    })
     if (dropdownContent.style.display === 'none'){
       dropdownContent.style.display = 'block';
       const buttons = dropdownContent.querySelectorAll('button');
